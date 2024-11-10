09be548b01c5e5cf53606326ecb4d8d8c0c17e6d
Jayce <jayce404@foxmail.com>
2024-11-10 20:02:16 +0800

    字符串拼接

--- chapter01/learn02_string.py
+++ chapter01/learn02_string.py
@@ -5,3 +5,12 @@ print(name.title());
 print(name.upper());
 # 完全小写
 print(name.lower());
+
+# 在字符串中使用变量
+first_name = "ada";
+last_name = "aba";
+# 在字符串中加入变量的值可以使用 f"…{}…"的形式进行插入，在{}中放入变量"
+# f是 format()函数的缩写
+print(f"Hello, {first_name} {last_name}!");
+# 等同于
+print("Hello, {0} {1}!".format(first_name, last_name));
