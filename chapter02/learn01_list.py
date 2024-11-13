2b91a928a68770e5faac5738e02e012ebbf28ca7
Jayce <jayce404@foxmail.com>
2024-11-13 20:25:02 +0800

    强迫症加入分号

--- chapter02/learn01_list.py
+++ chapter02/learn01_list.py
@@ -3,11 +3,11 @@ import logging.config
 import os
 
 # 获取当前文件名（不包括扩展名）
-current_filename = os.path.splitext(os.path.basename(__file__))[0]
+current_filename = os.path.splitext(os.path.basename(__file__))[0];
 # 加载日志配置文件
-logging.config.fileConfig('logging.conf')
+logging.config.fileConfig('logging.conf');
 # 获取logger对象，名称为当前文件名
-log = logging.getLogger(current_filename)
+log = logging.getLogger(current_filename);
 
 # 列表 list 是由一系列按特定顺序排列元素组成
 bicycle = ['trek', 'cannondale', 'redline', 'specialized'];
