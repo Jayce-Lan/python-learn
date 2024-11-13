61191b976f64154a3819db622a619b388f4e33fc
lanjs <jayce404@foxmail.com>
2024-11-13 12:07:05 +0800

    整数与浮点数

---
+++ chapter01/learn03_number.py
@@ -0,0 +1,21 @@
+# 整数integer
+# 整数可以进行 加+ 减- 乘* 除/ 操作；也支持像数学一样的计算顺序
+print(f"Output #1: {2 + 3}");
+print(f"Output #2: {2 - 3}");
+print(f"Output #3: {-2 * 3}");
+print(f"Output #4: {2 / 3}");
+# 乘方
+print(f"Output #5: {2 ** 3}");
+
+print(f"Output #6: {2 - 3 * 4}");
+print(f"Output #7: {(2 - 3) * 4}");
+
+# 浮点数(float)
+print(f"Output #8: {0.1 + 0.1}");
+print(f"Output #9: {0.2 + 0.2}");
+print(f"Output #10: {0.2 - 0.2}");
+print(f"Output #11: {0.2 - 0.1}");
+# 也会存在部分精度问题
+print(f"Output #12: {0.1 + 0.2}"); # 0.30000000000000004
+print(f"Output #13: {3 * 0.1}"); # 0.30000000000000004
+
