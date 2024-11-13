58ed83b1b0733ef0bc9b093a1ea2fde5dbe5ac04
lanjs <jayce404@foxmail.com>
2024-11-13 18:06:22 +0800

    添加日志打印，使用log的形式打印日志

--- chapter02/learn01_list.py
+++ chapter02/learn01_list.py
@@ -1,6 +1,17 @@
+import logging
+import logging.config
+import os
+
+# 获取当前文件名（不包括扩展名）
+current_filename = os.path.splitext(os.path.basename(__file__))[0]
+# 加载日志配置文件
+logging.config.fileConfig('logging.conf')
+# 获取logger对象，名称为当前文件名
+log = logging.getLogger(current_filename)
+
 # 列表 list 是由一系列按特定顺序排列元素组成
 bicycle = ['trek', 'cannondale', 'redline', 'specialized'];
-print(f"Output #1: {bicycle}");
+log.info(f"Output #1: {bicycle}");
 
 # 访问列表元素
-print(f"Output #2: {bicycle[0].title()}");
+log.info(f"Output #2: {bicycle[0].title()}");
