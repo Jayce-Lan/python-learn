9856a67988448fb66ad803c9b149d12321d963a8
lanjs <jayce404@foxmail.com>
2024-12-10 15:07:30 +0800

    DataFrame的操作-按行

--- chapter_pandas_data_analysis/learn01_dataframe_row.py
+++ chapter_pandas_data_analysis/learn01_dataframe_row.py
@@ -44,6 +44,11 @@ def test_get_data_frame_by_row():
 data_frame_row = test_get_data_frame_by_row()
 log.info(f'\n{data_frame_row}')
 
+
+################################
+# 索引的操作
+################################
+
 # 索引的名称可以进行手动设置
 data_frame_row.index.name = 'user_id'
 log.info(f'\n{data_frame_row}')
@@ -52,7 +57,45 @@ log.info(f'\n{data_frame_row}')
 # 此时，DataFrame会自动生成一个新的索引
 # 此方法需要一个新的DataFrame承接，原本的data_frame_row不会被改变
 data_frame_row_reset = data_frame_row.reset_index()
+
+# 也可以通过set_index('col_name')设置新的索引
+# 原本的data_frame_row不会被改变
+data_frame_row_reset = data_frame_row.reset_index().set_index('Name')
 log.info(f'\n{data_frame_row_reset}')
+log.info(f'\n{data_frame_row}')
 
+# 可以通过reindex(index_list)的方法批量更换索引
+# 但是这只会接管所有能匹配索引的行，匹配不到的会引入含有空值NaN的行
+# 被忽略的索引对应的行将会被丢弃，例如下面的例子只会匹配到 1001、1002
+data_frame_row_reset = data_frame_row.reindex([999, 1001, 1002, 1005])
+log.info(f'\n{data_frame_row_reset}')
+log.info(f'\n{data_frame_row}')
 
+################################
+# DataFrame的方法
+################################
 
+# DataFrame的方法返回的都是副本，不直接修改原本的DataFrame
+
+# 根据索引对DataFrame进行排序
+df = data_frame_row.sort_index()
+log.info(f'\n{df}')
+
+# 可以使用sort_values()对列进行排序
+
+# 单列排序，默认升序排列
+df = data_frame_row.sort_values('Name')
+log.info(f'\n{df}')
+# 传入ascending = False参数进行降序排列
+df = data_frame_row.sort_values('Name', ascending = False)
+log.info(f'\n{df}')
+
+# 多列排序，默认升序排列
+df = data_frame_row.sort_values(['Country', 'Score'])
+log.info(f'\n{df}')
+# 传入ascending = False参数进行降序排列
+# 或者传入ascending = [False, True] 进行首列降序，第二列升序的排列
+df = data_frame_row.sort_values(['Country', 'Score'], ascending = [False, True])
+log.info(f'\n{df}')
+df = data_frame_row.sort_values(['Country', 'Score'], ascending = False)
+log.info(f'\n{df}')
